5dfb1ed84b43b53b054a9f3643b1f643b13d5ae4
Joey B <Joeybrayshaw@gmail.com>
2021-01-03 10:16:48 -0800

    "Added print methods and expanded upon Steam"

--- gameScout.py
+++ gameScout.py
@@ -1,7 +1,7 @@
 import steam
 
-class Game():
 
+class Game:
     COUNTRY_CODE = "gb"
     CURRENCY = "GBP"
 
@@ -12,18 +12,40 @@ class Game():
             self.steam_id = str(name_query)
         else:
             self.steam_id = steam.get_app_id_by_query(name_query)
+
         self.title = steam.get_app_name(self.steam_id)
-        self.price = steam.get_app_price(self.steam_id, Game.COUNTRY_CODE)
+        self.base_price = steam.get_base_price(self.steam_id, Game.COUNTRY_CODE)
+
+        self.steam_price = steam.get_current_price(self.steam_id, Game.COUNTRY_CODE)
+        self.steam_discount = steam.get_discount_amount(self.steam_id, Game.COUNTRY_CODE)
+        self.steam_on_sale = self.steam_discount > 0
+
+        self.epic_price = None
+
+        self.gog_price = None
 
     def __str__(self):
         return self.title
 
     def __repr__(self):
-        return f"[{self.title}, {self.price}]"
+        return f"[{self.title}, {self.steam_price}]"
 
     # Get current prices function
+    def update(self):
+        self.steam_price = steam.get_current_price(self.steam_id, Game.COUNTRY_CODE)
+        self.steam_discount = steam.get_discount_amount(self.steam_id)
+        self.steam_on_sale = self.steam_discount > 0
 
     # Check against wishlist prices
+
+    # Print all info
+    def print_info(self):
+        print("*************************")
+        print(f"Game: {self.title}\tBase price: {self.base_price}")
+        print()
+        print(f"Steam")
+        print(f"On sale: {self.steam_on_sale}\tDiscount: {self.steam_discount}\tPrice: {self.steam_price}")
+        print("*************************")
 
 
 class Wishlist:
@@ -44,15 +66,18 @@ class Wishlist:
             if game.steam_id == game_to_delete.steam_id:
                 self.wishlist.remove(game)
 
-    def print_wishlist(self):
-        print(self.wishlist)
-
     def __str__(self):
         result = ""
         for item in self.wishlist:
             result += ", " + item.__repr__()
-        return(result.lstrip(", "))
+        return (result.lstrip(", "))
 
+    def print_game_info(self, index=int):
+        self.wishlist[index].print_info()
+
+    def print_all_game_info(self):
+        for game in self.wishlist:
+            game.print_info()
 
 
 w = Wishlist()
@@ -60,8 +85,8 @@ w = Wishlist()
 w.wishlist_add("Gears 5")
 w.wishlist_add("Warhammer total war")
 w.wishlist_add(1139900)
-
-w.wishlist_remove("Gears 5")
-w.wishlist_remove("Subnautica")
+w.wishlist_add("Persona 4")
 
 print(w)
+
+w.print_all_game_info()
--- steam.py
+++ steam.py
@@ -14,11 +14,24 @@ def get_app_name(game_id):
     soup = BeautifulSoup(res.content, "html.parser")
     return soup.find('div', class_="apphub_AppName").text
 
-def get_app_price(game_id, country_code):
+
+def get_current_price(game_id, country_code):
     url = "https://store.steampowered.com/api/appdetails?appids=" + game_id + "&cc=" + country_code
     res = requests.get(url)
     price = json.loads(res.content)[game_id]["data"]["price_overview"]['final'] / 100
     return price
 
 
+def get_base_price(game_id, country_code):
+    url = "https://store.steampowered.com/api/appdetails?appids=" + game_id + "&cc=" + country_code
+    res = requests.get(url)
+    price = json.loads(res.content)[game_id]["data"]["price_overview"]['initial'] / 100
+    return price
+
+
+def get_discount_amount(game_id, country_code):
+    url = "https://store.steampowered.com/api/appdetails?appids=" + game_id + "&cc=" + country_code
+    res = requests.get(url)
+    discount_amount = json.loads(res.content)[game_id]["data"]["price_overview"]["discount_percent"]
+    return discount_amount
 
